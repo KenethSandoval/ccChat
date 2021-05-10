63531ee4709cb0007f8b7ff6b89a13c37b7d69f5
Keneth Sandoval <kenethra.74@gmail.com>
2021-05-10 00:32:56 -0600

    feat: chat basic

--- Makefile
+++ Makefile
@@ -1,4 +1,10 @@
 CC = g++
 
+
+all: main client
+
 main: main.cpp
-		 $(CC) -o main main.cpp
+		 $(CC) -o main.out main.cpp
+
+client: client.cpp
+		 $(CC) -o client.out client.cpp
--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
-#include <iostream>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+struct sockaddr_in serv;
+int fd;
+int conn;
+char message[100] = "";
 
 int main() {
-  std::cout << "Hola mundo y adios";
+  serv.sin_family = AF_INET;
+  serv.sin_port = htons(8080);
+  serv.sin_addr.s_addr = INADDR_ANY;
+
+  fd = socket(AF_INET, SOCK_STREAM, 0);
+
+  bind(fd, (struct sockaddr *)&serv, sizeof(serv));
+
+  listen(fd, 5);
+
+  while(conn = accept(fd, (struct sockaddr *) NULL, NULL)) {
+    int pid;
+    if((pid = fork()) == 0) {
+      while(recv(conn, message, 100, 0)>0) {
+        printf("Message Received: %s\n", message);
+      }
+      exit(0);
+    }
+  }
   return 0;
 }
